9a9c09e5132a6f06fdaa8652f1585d6f581e88a1
Jason <samtiedt@gmail.com>
2020-03-17 11:15:19 -0500

    basic blog website outline

---
+++ src/build/precache-manifest.26afe455507d9a16abe183a90b696d88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4579a4124eea709b6978b11c0d8a48f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e48b7f4d233b066fe1b",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "61213f6377a1386114e9",
+    "url": "/static/js/2.fe96bb48.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.fe96bb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e48b7f4d233b066fe1b",
+    "url": "/static/js/main.0a042333.chunk.js"
+  },
+  {
+    "revision": "bf9cda129bd1b77590dc",
+    "url": "/static/js/runtime-main.162f5c2a.js"
+  }
+]);
